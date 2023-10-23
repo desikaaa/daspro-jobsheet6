f4e3851369db8f84faba87205b1d85ac8619bcd8
desikaaa <desikarmila211@gmail.com>
2023-10-23 21:10:53 +0700

    commit

---
+++ Modifikasi2Pemilihan2Percobaan107.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Modifikasi2Pemilihan2Percobaan107 {
+
+    public static void main(String[] args){
+        Scanner input07 = new Scanner(System.in);
+        int tahun;
+        System.out.println("Masukkan Tahun");
+        tahun = input07.nextInt();
+        if ((tahun % 4) == 0){
+            if((tahun % 100) !=0)
+            System.out.println("Tahun Kabisat");
+            else if ((tahun % 400) == 0)
+            System.out.println("Tahun kabisat");
+            else 
+            System.out.println("Tahun " +tahun+ " bukan tahun kabisat");
+
+        } else
+        System.out.println("Bukan Tahun Kabisat");
+    }
+}
